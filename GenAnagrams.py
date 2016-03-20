56c932054d02c3a141638e2ae753748bb2740080
mcgrath94 <mcgrath94@hotmail.com>
2016-03-20 22:48:29 +0000

    Updated generator
    updated the file witch generates the anagram to comply with countdown
    rules

--- GenAnagrams.py
+++ GenAnagrams.py
@@ -3,9 +3,15 @@
 import random
 import string
 
-#for now just generates any 9 letter word
-# -does not yet meet countdown game requirements
-rand = ''.join(random.choice(string.ascii_lowercase) for i in range(9)) 
+#generates 3 vowels, 4 cons, and 2 of any as per Countdown rules
+vowels = ('a','o','u','e','i')
+consonant = ('b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z')
+
+randVow = ''.join(random.choice(vowels) for i in range(3))
+randCon = ''.join(random.choice(consonant) for i in range(4))  
+randAny = ''.join(random.choice(string.ascii_lowercase) for i in range(2)) 
+
+rand = randVow + randCon + randAny
 print (rand)
 
 input('Press ENTER to exit') #to keep window open
