c316d8be2ceedb015ab4cd01aa3d3d12275d819a
mcgrath94 <mcgrath94@hotmail.com>
2016-03-22 00:15:25 +0000

    outputs only 9 letter words, if none then 8, 7 etc.

--- countdown-solver.py
+++ countdown-solver.py
@@ -3,14 +3,15 @@ from itertools import permutations
 import random
 import string
 
-#generates 3 vowels, 4 cons, and 2 of any as per Countdown rules
 vowels = ('a','o','u','e','i')
 consonant = ('b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z')
 
+#generates 3 vowels, 4 cons, and 2 of any as per Countdown rules
 randVow = ''.join(random.choice(vowels) for i in range(3))
 randCon = ''.join(random.choice(consonant) for i in range(4))  
 randAny = ''.join(random.choice(string.ascii_lowercase) for i in range(2)) 
 
+#joins the 3 vowels, 4 cons and other 2 letters
 rand = randVow + randCon + randAny
 print ('Random letters: ', rand)
 
@@ -25,9 +26,21 @@ perms = ([''.join(p) for i in range(1, len(rand)+1) for p in permutations(rand, 
 #also prints all possible words, will modify to print only longest
 print ('\n---Anagrams--- ')
 def main():
+    #prints only words that are in perms and in words
     for x in perms:
-        if x in words: 
-               print(x)
+        if x in words:
+            #outputs 9 letter words - if none, then 8, 7...
+            #will change with a for loop or something similar
+            if len(x)>8:
+                print (x)
+            elif len(x)>7:
+                print (x)
+            elif len(x)>6:
+                print (x)
+            elif len(x)>5:
+                print (x)
+            elif len(x)>4:
+                print (x)
 
 main()
 
